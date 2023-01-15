d2f46c23002c1681665220cc99bf9210a1895d0d
amanchoudhary18 <aman.choudhary9785@gmail.com>
2023-01-15 18:32:25 +0530

    Initalize

--- config.js
+++ config.js
@@ -1,29 +1,24 @@
-const { initializeApp }= require('firebase/app');
-const { getFirestore, addDoc, collection  } = require('firebase/firestore');
+const { initializeApp } = require("firebase/app");
+const { getFirestore, addDoc, collection } = require("firebase/firestore");
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceKey.json");
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-    apiKey: "AIzaSyDqJUlb2SdUlkPtt4FAtpJ_TfvziU0QlOY",
-
-    authDomain: "sweetscape.firebaseapp.com",
-  
-    databaseURL: "https://sweetscape-default-rtdb.firebaseio.com",
-  
-    projectId: "sweetscape",
-  
-    storageBucket: "sweetscape.appspot.com",
-  
-    messagingSenderId: "770966537264",
-  
-    appId: "1:770966537264:web:7bb629e9a1d35918e859f5"
-  
+  apiKey: "AIzaSyAwumtb4VCuJQZKvXxRQouUViidleM_AEQ",
+  authDomain: "sweetscapes-47eb3.firebaseapp.com",
+  projectId: "sweetscapes-47eb3",
+  storageBucket: "sweetscapes-47eb3.appspot.com",
+  messagingSenderId: "930049639322",
+  appId: "1:930049639322:web:2dd3eb33bbfb5f6d98f023",
+  measurementId: "G-6WDPSSFX7H",
 };
-
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+const firestore = admin.firestore();
 
 // Initialize Cloud Firestore and get a reference to the service
-const db = getFirestore(app);
-exports.db=db
+exports.db = firestore;
--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-const express = require('express');
+const express = require("express");
+const admin = require("firebase-admin");
 const app = express();
-const {db}=require("./config")
+const { db } = require("./config");
 
 //Port to serve
 const PORT = process.env.PORT || 3000;
 
 //Routes
-app.get('/', (req, res) => {
-  res.send('Welcome to Node Express Server');
+app.get("/", (req, res) => {
+  res.send("Welcome to Node Express Server");
 });
 
-app.post('/createUser', async(req,res)=>{
+app.post("/createUser", async (req, res) => {
   try {
-    const docRef = await addDoc(collection(db, "users"), {
-      first: "Alan",
-      middle: "Mathison",
-      last: "Turing",
-      born: 1912
+    const docRef = await db.collection("Users").doc("1").set({
+      ID: "1",
+      Name: "Aman",
+      Age: "20",
     });
-  
-    console.log("Document written with ID: ", docRef.id);
+    res.status(200).send(docRef);
+
+    console.log("Document written with ID: ", docRef.ID);
   } catch (e) {
     console.error("Error adding document: ", e);
+    res.status(500).send(e);
   }
-})
+});
 
 //Start the server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
