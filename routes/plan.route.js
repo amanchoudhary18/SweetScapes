64ce5cea3987bee975e04143ccc75604bf221b33
amanchoudhary18 <aman.choudhary9785@gmail.com>
2023-08-13 13:52:34 +0530

    console

--- routes/plan.route.js
+++ routes/plan.route.js
@@ -530,7 +530,7 @@ router.post("/createPlan", async (req, res) => {
     nearestBusTime.setHours(startBus[0].boarding.arrival_time.split(":")[0]);
     nearestBusTime.setMinutes(startBus[0].boarding.arrival_time.split(":")[1]);
 
-    console.log(nearestBusTime, time);
+    console.log(nearestBusTime, moment.tz(time, "Asia/Kolkata").toDate());
 
     let start_bus_found = true;
     let end_bus_found = true;
